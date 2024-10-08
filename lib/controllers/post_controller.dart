fad8753dbb3052d7e976cf6329df4437a4fbab9c
CEFab <46172236+CEFab@users.noreply.github.com>
2024-10-08 16:59:08 -0400

    code some of the likeDislikeComment method

--- lib/controllers/post_controller.dart
+++ lib/controllers/post_controller.dart
@@ -173,4 +173,33 @@ class PostController extends GetxController {
       print(e.toString());
     }
   }
+
+  Future likeAndDislike(id) async {
+    try {
+      isLoading.value = true;
+      var request = await http.post(
+        Uri.parse('${url}feed/comment/$id'),
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Bearer ${box.read('token')}',
+          // 'Bearer token': box.read('token'),
+        },
+      );
+
+      if (request.statusCode == 200 ||
+          json.decode(request.body)['message'] == 'liked') {
+        isLoading.value = false;
+        print(json.decode(request.body));
+      } else if (request.statusCode == 200 ||
+          json.decode(request.body)['message'] == 'Unliked') {
+        isLoading.value = false;
+        print(json.decode(request.body));
+      } else {
+        isLoading.value = false;
+        print(json.decode(request.body));
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
